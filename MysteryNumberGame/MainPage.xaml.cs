2bff6008564bc07957d721a49dea7da7d048f829
tmnpierre <tomyn.pierre@gmail.com>
2024-02-19 16:11:30 +0100

    feat(mainpage): ajoute jeu du nombre mystère avec UI et logique

---
+++ MysteryNumberGame/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace MysteryNumberGame
+{
+    public partial class MainPage : ContentPage
+    {
+        int mysteryNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        void InitializeGame()
+        {
+            Random random = new Random();
+            mysteryNumber = random.Next(1, 101);
+        }
+
+        private void OnCheckClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(userInput.Text, out int userGuess))
+            {
+                if (userGuess == mysteryNumber)
+                {
+                    resultLabel.Text = "Bravo ! Vous avez trouvé le nombre mystère.";
+                    resultLabel.TextColor = Colors.Green;
+                    InitializeGame();
+                }
+                else if (userGuess < mysteryNumber)
+                {
+                    resultLabel.Text = "Le nombre mystère est plus grand.";
+                    resultLabel.TextColor = Colors.Yellow;
+                }
+                else
+                {
+                    resultLabel.Text = "Le nombre mystère est plus petit.";
+                    resultLabel.TextColor = Colors.Orange;
+                }
+            }
+            else
+            {
+                resultLabel.Text = "Veuillez entrer un nombre valide.";
+                resultLabel.TextColor = Colors.Red;
+            }
+        }
+    }
+}
---
+++ MysteryNumberGame/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MysteryNumberGame
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
